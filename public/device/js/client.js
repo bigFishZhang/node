1f152c6305bf4c478c9e920b60df7f8f8d704194
zzbbigfish <zzbbigfish@gmail.com>
2019-05-12 16:14:31 +0800

    获取设备列表

--- public/device/js/client.js
+++ public/device/js/client.js
@@ -1,5 +1,9 @@
 'use strict'
 
+var audioSource = document.querySelector('select#audioSource');
+var audioOutput = document.querySelector('select#audioOutput');
+var videoSource = document.querySelector('select#videoSource');
+
 if(!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) 
 {
     console.log('enumerateDevices is not supported! \n');
@@ -16,6 +20,18 @@ function gotDevices(deviceInfos)
                     + deviceInfo.label + " ; \n id = "
                     + deviceInfo.deviceId + "  ; \n groupId ="
                     + deviceInfo.groupId);
+        var option = document.createElement('option');
+        option.text =  deviceInfo.label;
+        option.value = deviceInfo.deviceId;
+
+        if(deviceInfo.kind === 'audioinput'){
+            audioSource.appendChild(option);
+        }else if(deviceInfo.kind === 'audiooutput'){
+            audioOutput.appendChild(option);
+        }else if(deviceInfo.kind === 'videoinput'){
+            videoSource.appendChild(option);
+        }
+
     });
 }
 
