b9a015f6ad3aa7d49ae95b1b1051eee0db4b0946
zzbbigfish <zzbbigfish@gmail.com>
2019-05-23 16:21:58 +0800

    添加录制功能

--- public/mediaStream/js/client.js
+++ public/mediaStream/js/client.js
@@ -8,59 +8,70 @@ var videoSource = document.querySelector('select#videoSource')
 var filtersSelect = document.querySelector('select#filter')
 var snapshotButton = document.querySelector('button#snapshot')
 var pictureCanvas = document.querySelector('canvas#picture')
-
 var constraints = document.querySelector('div#constraints')
 
-pictureCanvas.width = 320
-pictureCanvas.height= 240
-// 获取设备
-function gotDevices(deviceInfos)
-{
-    deviceInfos.forEach(function(deviceInfo){
-          var option = document.createElement('option');
-          option.text = deviceInfo.label
-          option.value = deviceInfo.deviceId;
+//record
+var recVideo = document.querySelector('video#recplayer')
+var recordBtn = document.querySelector('button#record')
+var playBtn = document.querySelector('button#recplay')
+var downloadBtn = document.querySelector('button#download')
 
-           if (deviceInfo.kind === "audioinput"){
+//buffer
+var buffer;
+var mediaRecorder;
+
+
+pictureCanvas.width = 320
+pictureCanvas.height = 240
+// 获取设备
+function gotDevices(deviceInfos) {
+    deviceInfos.forEach(function (deviceInfo) {
+        var option = document.createElement('option');
+        option.text = deviceInfo.label
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === "audioinput") {
             audioSource.appendChild(option)
 
-           }else if(deviceInfo.kind === "audiooutput"){
+        } else if (deviceInfo.kind === "audiooutput") {
             audioOutput.appendChild(option)
-           }else if(deviceInfo.kind === "videoinput"){
+        } else if (deviceInfo.kind === "videoinput") {
             0
-           }
+        }
     })
 }
 //获取音视频流
-function gotMediaStream(stream)
-{
-    videoplay.srcObject = stream;
+function gotMediaStream(stream) {
+
     var videoTrack = stream.getVideoTracks()[0];
-    var videoConstraints =  videoTrack.getSettings();
-    
-    constraints.textContent =  JSON.stringify(videoConstraints,null,2);
+    var videoConstraints = videoTrack.getSettings();
+
+    constraints.textContent = JSON.stringify(videoConstraints, null, 2);
+    //全局变量
+    window.stream = stream;
+
+    videoplay.srcObject = stream;
 
     // audioplay.srcObject =  stream;
     return navigator.mediaDevices.enumerateDevices();
 }
-function handleError(err)
-{
+
+function handleError(err) {
     console.log('gotMediaStream  is not supported! \n');
 }
 
-function start()
-{
+function start() {
 
-    if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
         console.log('getUserMedia is not supported! \n');
         return
-    }else{
+    } else {
         var deviceId = videoSource.value
         var constraints = {
-            video : {
-                width:320,
-                height:240,
-                frameRate:30,
+            video: {
+                width: 320,
+                height: 240,
+                frameRate: 30,
                 // width:{
                 //     min:640,
                 //     max:1080
@@ -77,37 +88,97 @@ function start()
 
                 //设置deviceId
                 deviceId: deviceId ? deviceId : undefined
-    
+
             },
             // video : true,
             // audio : true
-            audio : {
-                noiseSuppression:true,//降噪
-                echoCancellation:true,//回音消除
-                autoGainControl:true  //自动增益
+            audio: {
+                noiseSuppression: true, //降噪
+                echoCancellation: true, //回音消除
+                autoGainControl: true //自动增益
             }
         }
         //获取音视频信息
         navigator.mediaDevices.getUserMedia(constraints)
-                              .then(gotMediaStream)
-                              .then(gotDevices)
-                              .catch(handleError);
-        
+            .then(gotMediaStream)
+            .then(gotDevices)
+            .catch(handleError);
+
     }
 }
 
 //启动
 start();
 
-videoSource.onchange  = start;
-filtersSelect.onchange = function()
-{
+videoSource.onchange = start;
+filtersSelect.onchange = function () {
     videoplay.className = filtersSelect.value
 
 }
-snapshotButton.onclick = function() 
-{
+snapshotButton.onclick = function () {
     pictureCanvas.className = filtersSelect.value
-    pictureCanvas.getContext('2d').drawImage(videoplay,0,0,pictureCanvas.width,pictureCanvas.height)
+    pictureCanvas.getContext('2d').drawImage(videoplay, 0, 0, pictureCanvas.width, pictureCanvas.height)
 
+}
+// 开始录制
+function startRecord() {
+    //用于存储录制的内容
+    buffer = [];
+
+    var options = {
+        mimeType: 'video/webm;codecs=vp8'
+    }
+    //判断浏览器是否支持options.mimeType
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error('${options.mimeType} is not supported!')
+        return;
+    }
+    try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+        console.error('Failed to create mediaRecorder:', e);
+        return;
+    }
+    //录制的数据是否存储
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    //启动录制 时间片 每个一个时间片存储一个数据
+    mediaRecorder.start(10);
+
+
+}
+// 停止录制
+function stopRecord() {
+    mediaRecorder.stop();
+}
+// 存储录制的数据
+function handleDataAvailable(e) {
+    if (e && e.data && e.data.size > 0) {
+        buffer.push(e.data);
+    }
+
+}
+
+recordBtn.onclick = function () {
+    if (recordBtn.textContent === 'Start Record') {
+        startRecord();
+        recordBtn.textContent = 'Stop Record';
+        playBtn.disabled = true;
+        downloadBtn.disabled = true;
+    } else {
+        stopRecord();
+        recordBtn.textContent = 'Start Record';
+        playBtn.disabled = false;
+        downloadBtn.disabled = false;
+    }
+}
+
+playBtn.onclick = function () {
+    var blob = new Blob(buffer, {
+        type: 'video/webm'
+    });
+    recVideo.src = window.URL.createObjectURL(blob);
+    //获取直播流数据的时候 stream 复制给recVideo.srcObject 
+    recVideo.srcObject = null;
+    recVideo.controls = true;
+    recVideo.play();
 }
