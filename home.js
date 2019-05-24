4c39e94ba5fb64d4d54a390366c28e3f7103cd06
zzbbigfish <zzbbigfish@gmail.com>
2019-05-24 10:21:40 +0800

    server socket.io support http listen

--- home.js
+++ home.js
@@ -39,6 +39,48 @@ app.use(express.static('./public'));
 
 //http server
 var http_server = http.createServer(app);
+//bind socketio with http_server
+var sockio = socketIo.listen(http_server);
+//connection
+sockio.sockets.on('connection', (socket) => {
+  //接收消息
+  socket.on('message', (room, data) => {
+    socket.to(room).emit('message', room, data); //给房间所有人发消息（除自己）
+  });
+  //此处应该加锁
+  socket.on('join', (room) => {
+    socket.join(room);
+    var myRoom = io.sockets.adapter.rooms[room];
+    var users = Object.keys(myRoom.sockets).length;
+
+    logger.log('the number of user in room is :' + users);
+
+    //控制房间人数
+    if (users < 4) {
+      socket.emit('joined', room, socket.id);
+      if (users > 1) {
+        socket.to(room).emit('otherjoin', room); //给除自己外发消息
+      }
+    } else {
+      socket.leave(room);
+      socket.emit('full', room, socket.io);
+    }
+    // socket.to(room).emit('joined', room, socket.id);//房间内除自己之外
+    // io.in(room).emit('joined', room, socket.id);//房间内所有人
+    // socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
+  });
+  socket.on('leaver', (room) => {
+    var myRoom = io.sockets.adapter.rooms[room];
+    var users = Object.keys(myRoom.sockets).length;
+    logger.log('the number of user in room is :' + (users - 1));
+    socket.leave(room);
+    socket.to(room).emit('bye', room, socket.id); //给房间所有人（自己除外）
+    socket.emit('leaved', room, socket.id); //给自己
+  });
+
+});
+
+
 http_server.listen(80, '0.0.0.0');
 
 //https server
