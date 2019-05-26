715840ac6b7935d433789cd06c84c4db08eec03e
zzbbigfish <zzbbigfish@gmail.com>
2019-05-26 18:01:45 +0800

    add p2p 1 to 1 localhost

--- public/mediaStream/js/client.js
+++ public/mediaStream/js/client.js
@@ -25,87 +25,86 @@ pictureCanvas.width = 320
 pictureCanvas.height = 240
 // 获取设备
 function gotDevices(deviceInfos) {
-    deviceInfos.forEach(function (deviceInfo) {
-        var option = document.createElement('option');
-        option.text = deviceInfo.label
-        option.value = deviceInfo.deviceId;
+  deviceInfos.forEach(function (deviceInfo) {
+    var option = document.createElement('option');
+    option.text = deviceInfo.label
+    option.value = deviceInfo.deviceId;
 
-        if (deviceInfo.kind === "audioinput") {
-            audioSource.appendChild(option)
+    if (deviceInfo.kind === "audioinput") {
+      audioSource.appendChild(option)
 
-        } else if (deviceInfo.kind === "audiooutput") {
-            audioOutput.appendChild(option)
-        } else if (deviceInfo.kind === "videoinput") {
-            videoSource.appendChild(option)
-        }
-    })
+    } else if (deviceInfo.kind === "audiooutput") {
+      audioOutput.appendChild(option)
+    } else if (deviceInfo.kind === "videoinput") {
+      videoSource.appendChild(option)
+    }
+  })
 }
 //获取音视频流
 function gotMediaStream(stream) {
 
-    var videoTrack = stream.getVideoTracks()[0];
-    var videoConstraints = videoTrack.getSettings();
+  var videoTrack = stream.getVideoTracks()[0];
+  var videoConstraints = videoTrack.getSettings();
 
-    constraints.textContent = JSON.stringify(videoConstraints, null, 2);
-    //全局变量
-    window.stream = stream;
+  constraints.textContent = JSON.stringify(videoConstraints, null, 2);
+  //全局变量
+  window.stream = stream;
 
-    videoplay.srcObject = stream;
+  videoplay.srcObject = stream;
 
-    // audioplay.srcObject =  stream;
-    return navigator.mediaDevices.enumerateDevices();
+  // audioplay.srcObject =  stream;
+  return navigator.mediaDevices.enumerateDevices();
 }
 
 function handleError(err) {
-    console.log('gotMediaStream  is not supported! \n');
+  console.log('gotMediaStream  is not supported! \n');
 }
 
 function start() {
-    //if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getDisplayMedia) {
-        console.log('getUserMedia is not supported! \n');
-        return
-    } else {
-        var deviceId = videoSource.value
-        var constraints = {
-            video: {
-                width: 320,
-                height: 240,
-                frameRate: 30,
-                // width:{
-                //     min:640,
-                //     max:1080
-                // },
-                // height:{
-                //     min:480,
-                //     max:1920
-                // },
-                // frameRate:{
-                //     min:15,
-                //     max:60
-                // },
-                // facingMode:'enviroment', // user // 摄像头前后置 默认前置
+  //if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getDisplayMedia) {
+    console.log('getUserMedia is not supported! \n');
+    return
+  } else {
+    var deviceId = videoSource.value
+    var constraints = {
+      video: {
+        width: 320,
+        height: 240,
+        frameRate: 30,
+        // width:{
+        //     min:640,
+        //     max:1080
+        // },
+        // height:{
+        //     min:480,
+        //     max:1920
+        // },
+        // frameRate:{
+        //     min:15,
+        //     max:60
+        // },
+        // facingMode:'enviroment', // user // 摄像头前后置 默认前置
 
-                //设置deviceId
-                deviceId: deviceId ? deviceId : undefined
+        //设置deviceId
+        deviceId: deviceId ? deviceId : undefined
 
-            },
-            // video : true,
-            // audio : true
-            audio: {
-                noiseSuppression: true, //降噪
-                echoCancellation: true, //回音消除
-                autoGainControl: true //自动增益
-            }
-        }
-        //获取音视频信息 //
-        // navigator.mediaDevices.getUserMedia(constraints)
-        navigator.mediaDevices.getDisplayMedia(constraints)
-            .then(gotMediaStream)
-            .then(gotDevices)
-            .catch(handleError);
-
+      },
+      // video : true,
+      // audio : true
+      audio: {
+        noiseSuppression: true, //降噪
+        echoCancellation: true, //回音消除
+        autoGainControl: true //自动增益
+      }
     }
+    //获取音视频信息 //
+    // navigator.mediaDevices.getUserMedia(constraints)
+    navigator.mediaDevices.getDisplayMedia(constraints)
+      .then(gotMediaStream)
+      .then(gotDevices)
+      .catch(handleError);
+  }
 }
 
 //启动
@@ -113,85 +112,85 @@ start();
 
 videoSource.onchange = start;
 filtersSelect.onchange = function () {
-    videoplay.className = filtersSelect.value
+  videoplay.className = filtersSelect.value
 
 }
 snapshotButton.onclick = function () {
-    pictureCanvas.className = filtersSelect.value
-    pictureCanvas.getContext('2d').drawImage(videoplay, 0, 0, pictureCanvas.width, pictureCanvas.height)
+  pictureCanvas.className = filtersSelect.value
+  pictureCanvas.getContext('2d').drawImage(videoplay, 0, 0, pictureCanvas.width, pictureCanvas.height)
 
 }
 // 开始录制
 function startRecord() {
-    //用于存储录制的内容
-    buffer = [];
+  //用于存储录制的内容
+  buffer = [];
 
-    var options = {
-        mimeType: 'video/webm;codecs=vp8'
-    }
-    //判断浏览器是否支持options.mimeType
-    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-        console.error('${options.mimeType} is not supported!')
-        return;
-    }
-    try {
-        mediaRecorder = new MediaRecorder(window.stream, options);
-    } catch (e) {
-        console.error('Failed to create mediaRecorder:', e);
-        return;
-    }
-    //录制的数据是否存储
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    //启动录制 时间片 每个一个时间片存储一个数据
-    mediaRecorder.start(10);
+  var options = {
+    mimeType: 'video/webm;codecs=vp8'
+  }
+  //判断浏览器是否支持options.mimeType
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error('${options.mimeType} is not supported!')
+    return;
+  }
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Failed to create mediaRecorder:', e);
+    return;
+  }
+  //录制的数据是否存储
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  //启动录制 时间片 每个一个时间片存储一个数据
+  mediaRecorder.start(10);
 
 
 }
 // 停止录制
 function stopRecord() {
-    mediaRecorder.stop();
+  mediaRecorder.stop();
 }
 // 存储录制的数据
 function handleDataAvailable(e) {
-    if (e && e.data && e.data.size > 0) {
-        buffer.push(e.data);
-    }
+  if (e && e.data && e.data.size > 0) {
+    buffer.push(e.data);
+  }
 
 }
 
 recordBtn.onclick = function () {
-    if (recordBtn.textContent === 'Start Record') {
-        startRecord();
-        recordBtn.textContent = 'Stop Record';
-        playBtn.disabled = true;
-        downloadBtn.disabled = true;
-    } else {
-        stopRecord();
-        recordBtn.textContent = 'Start Record';
-        playBtn.disabled = false;
-        downloadBtn.disabled = false;
-    }
+  if (recordBtn.textContent === 'Start Record') {
+    startRecord();
+    recordBtn.textContent = 'Stop Record';
+    playBtn.disabled = true;
+    downloadBtn.disabled = true;
+  } else {
+    stopRecord();
+    recordBtn.textContent = 'Start Record';
+    playBtn.disabled = false;
+    downloadBtn.disabled = false;
+  }
 }
 
 playBtn.onclick = function () {
-    var blob = new Blob(buffer, {
-        type: 'video/webm'
-    });
-    recVideo.src = window.URL.createObjectURL(blob);
-    //获取直播流数据的时候 stream 复制给recVideo.srcObject 
-    recVideo.srcObject = null;
-    recVideo.controls = true;
-    recVideo.play();
+  var blob = new Blob(buffer, {
+    type: 'video/webm'
+  });
+  recVideo.src = window.URL.createObjectURL(blob);
+  //获取直播流数据的时候 stream 复制给recVideo.srcObject 
+  recVideo.srcObject = null;
+  recVideo.controls = true;
+  recVideo.play();
 }
 
 downloadBtn.onclick = function () {
-    var blob = new Blob(buffer, {
-        type: 'video/webm'
-    });
-    var url = window.URL.createObjectURL(blob);
-    var a = document.createElement('a');
-    a.href = url;
-    a.style.display = 'none';
-    a.download = 'text.webm';
-    a.click();
+  var blob = new Blob(buffer, {
+    type: 'video/webm'
+  });
+  var url = window.URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.href = url;
+  a.style.display = 'none';
+  a.download = 'text.webm';
+  a.click();
 }
---
+++ public/peerConnection/js/main.js
@@ -0,0 +1,138 @@
+'use strict'
+// 视频
+var localVideo = document.querySelector('video#localvideo');
+var remoteVideo = document.querySelector('video#remotevideo');
+//按钮
+var startBtn = document.querySelector('button#start');
+var callBtn = document.querySelector('button#call');
+var hangupBtn = document.querySelector('button#hangup');
+//显示信息
+var offerSdpTextarea = document.querySelector('textarea#offer');
+var answerSdpTextarea = document.querySelector('textarea#answer');
+
+//全局变量
+var localStream;
+var pc1;
+var pc2;
+
+// 设置本地视频流播放
+function getMediaStream(stream) {
+  localVideo.srcObject = stream;
+  localStream = stream;
+}
+
+function handleError(err) {
+  console.error('Failed to get Media stream!', err);
+}
+
+// 获取浏览器流信息，输入到localVideo
+function start() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    console.error('GetuserMedia is not supported in this browser! ');
+    return;
+  } else {
+    var constraints = {
+      video: true,
+      audio: false
+    }
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(getMediaStream)
+      .catch(handleError);
+
+    startBtn.disabled = true;
+    callBtn.disabled = false;
+    hangupBtn.disabled = false;
+  }
+}
+
+//pc2响应ontrack事件
+function getRemoteStream(e) {
+  remoteVideo.srcObject = e.streams[0];
+}
+
+// create Answer后 设置setLocalDescription
+function getAnswer(desc) {
+  //pc2 setLocalDescription
+  pc2.setLocalDescription(desc);
+  //显示sdp
+  answerSdpTextarea.value = desc.sdp;
+
+  //send desc to signal
+
+  //receive desc from signal
+
+  pc1.setRemoteDescription(desc);
+
+}
+
+function handleAnswerError(err) {
+  console.error('Failed to create answer:', err);
+}
+
+//create Offer事件后把信息设置到本地LocalDescription 
+function getOffer(desc) {
+  pc1.setLocalDescription(desc);
+  //显示sdp 信息
+  offerSdpTextarea.value = desc.sdp;
+
+  //send desc to signal
+
+  //receive desc from signal
+
+  //pc2 setRemoteDescription
+  pc2.setRemoteDescription(desc);
+
+  //pc3 create answer
+  pc2.createAnswer().then(getAnswer).catch(handleAnswerError);
+
+}
+
+function handleOfferError(err) {
+  console.error('Failed to create offer:', err);
+}
+//创建peerConnection
+function call() {
+  pc1 = new RTCPeerConnection();
+  pc2 = new RTCPeerConnection();
+
+  // pc1 pc2 监听到 onicecandidate 事件添加候选者信息
+  pc1.onicecandidate = function (e) {
+    pc2.addIceCandidate(e.candidate);
+  }
+  pc2.onicecandidate = function (e) {
+    pc1.addIceCandidate(e.candidate);
+  }
+
+  // pc2 onTrack 信息监听处理
+  pc2.ontrack = getRemoteStream;
+
+  //pc1 添加本地流的 track
+  localStream.getTracks().forEach((track) => {
+    pc1.addTrack(track, localStream);
+  });
+
+  //pc1 create offer
+  var offerOptions = {
+    offerToRecieveAudio: 0,
+    offerToRecieveVideo: 1
+  }
+  pc1.createOffer(offerOptions).then(getOffer).catch(handleOfferError);
+
+  callBtn.disabled = true;
+  hangupBtn.disabled = false;
+}
+
+function hangup() {
+  pc1.close();
+  pc2.close();
+  // pc1 = null;
+  // pc2 = null;
+
+  callBtn.disabled = false;
+  hangup.disabled = true;
+}
+
+//按钮事件响应
+startBtn.onclick = start;
+callBtn.onclick = call;
+hangupBtn.onclick = hangup;
